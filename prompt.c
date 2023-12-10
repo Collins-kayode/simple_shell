50740f628dc26ceb88956ba7286e9d069929c0a8
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-12-10 14:50:16 -0800

    pls recheck the prompt

--- prompt.c
+++ prompt.c
@@ -1,11 +1,20 @@
 #include "shell.h"
 /**
- * main - void
+ * main - Entry point
  *
  * Return: always 0
  */
 int main (void)
 {
+	int i = 5;
+	int j = 6;
+	
+	if (i == 0, i < j, i++)
+		printf("my betty is good %d\n", i);
+	else
+	{
+		printf("Am not good betty %d\n", i);
+	}
 	return (0);
 }
 
