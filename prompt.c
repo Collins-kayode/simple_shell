6c8ad6f629225ff1e34754d92ad4368aa50dbf3a
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-12-10 22:51:32 -0800

    pls check

---
+++ prompt.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * _prompt - The prompt line in shell
+ * @argv: first parameter for argument
+ * @argc: second parameter for argument
+ * Return: return NULL if not return 0
+ */
+int _prompt(argv, *argc)
+{
+	return (0);
+}
+
